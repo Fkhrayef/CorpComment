565d2f3d89d80c8d741d37e14b90fc39ef0f1574
Fkhrayef <alkhrayefff@gmail.com>
2025-02-25 21:48:07 +0300

    first commit

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,19 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <small>
+        <p>
+          &copy; Copyright by{" "}
+          <a target="_blank" href="https://github.com/Fkhrayef">
+            Fkhrayef
+          </a>
+          . Intended for learning React with TypeScript!
+        </p>
+        <p>
+          <span className="u-bold u-italic">Feel free</span> to play with it,
+          tweak it, or even redesigning it!
+        </p>
+      </small>
+    </footer>
+  );
+}
